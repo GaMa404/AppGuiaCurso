14b41a34d27dfbe7c5c53d32d01d51ae6696de8a
GaMa404 <luzettileonardo@gmail.com>
2022-09-28 15:51:49 -0300

    first commit

--- AppGuiaCurso/AppGuiaCurso/MainPage.xaml.cs
+++ AppGuiaCurso/AppGuiaCurso/MainPage.xaml.cs
@@ -6,13 +6,95 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
+using AppGuiaCurso.View;
+
 namespace AppGuiaCurso
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : MasterDetailPage
     {
         public MainPage()
         {
             InitializeComponent();
+
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Inicial)));
+        }
+
+        private async void Open_Inicial(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Inicial)));
+                IsPresented = false;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro!", ex.Message, "OK");
+            }
+        }
+
+        private async void Open_Primeiro(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(ComponentesPrimeiro)));
+                IsPresented = false;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro!", ex.Message, "OK");
+            }
+        }
+
+        private async void Open_Segundo(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(ComponentesSegundo)));
+                IsPresented = false;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro!", ex.Message, "OK");
+            }
+        }
+
+        private async void Open_Terceiro(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(ComponentesTerceiro)));
+                IsPresented = false;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro!", ex.Message, "OK");
+            }
+        }
+
+        private async void Open_Vestibulinho(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Vestibulinho)));
+                IsPresented = false;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro!", ex.Message, "OK");
+            }
+        }
+
+        private async void Open_Contato(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Contato)));
+                IsPresented = false;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro!", ex.Message, "OK");
+            }
         }
     }
 }
---
+++ AppGuiaCurso/AppGuiaCurso/Model/Componentes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppGuiaCurso.Model
+{
+    internal class Componentes
+    {
+        public string Nome { get; set; }
+        public int CargaHoraria { get; set; }
+        public string AtribuicoesResponsabilidades { get; set; }
+        public string ValoresAtitudes { get; set; }
+        public string Competencias { get; set; }
+        public string Habilidades { get; set; }
+        public string BasesTecnologicas { get; set; }
+    }
+}
